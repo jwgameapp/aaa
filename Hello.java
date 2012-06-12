362580772d867c71afb56e5081a234f85277298c
jwgameapp <jwgameapp@qq.com>
2012-06-12 18:06:37 +0800

    1.commit all files

---
+++ Hello.java
@@ -0,0 +1,15 @@
+public class Hello
+{
+
+	/**
+	 * Implement the application
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
